1ca162fd05b269787a7ced2c0a9097bd6abc480f
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2021-03-27 16:56:16 +0500

    added utlility functions

--- query.py
+++ query.py
@@ -1,6 +1,9 @@
 import nltk
 import re
 
+totalNumberOfDocuments = 50
+
+# Read and parse all stop words from file
 fp = open('./Stopword-List.txt','r')
 stopWords = fp.read()
 stopWords = nltk.word_tokenize(stopWords)
@@ -9,7 +12,7 @@ fp.close()
 stemmer = nltk.stem.porter.PorterStemmer()
 punctuationRegex = "[.,!?:;‘’”“\"]"
 dictionary = {}
-i=1
+i = 1
 
 while(i<=45):
   fp = open('./ShortStories/{}.txt'.format(i), 'r')
@@ -35,4 +38,127 @@ while(i<=45):
 
   i += 1
 
-print(dictionary)
+def singleTermQuery(__term):
+  __term = __term.lower()
+  stemmedTerm = stemmer.stem(__term)
+  return (dictionary[stemmedTerm])
+
+def ORQuery(__firstTerm, __secondTerm):
+  firstTermDocuments = []
+  secondTermDocuments = []
+
+  for document in singleTermQuery(__firstTerm):
+    firstTermDocuments.append(document)
+
+  for document in singleTermQuery(__secondTerm):
+    secondTermDocuments.append(document)
+
+  totalDocuments = []
+  i = 0
+  j = 0
+
+  while(i < len(firstTermDocuments) and j < len(secondTermDocuments)):
+    if(firstTermDocuments[i] == secondTermDocuments[j]):
+      totalDocuments.append(firstTermDocuments[i])
+      i += 1
+      j += 1
+    elif(firstTermDocuments[i] < secondTermDocuments[j]):
+      totalDocuments.append(firstTermDocuments[i])
+      i += 1
+    else:
+      totalDocuments.append(secondTermDocuments[j])
+      j += 1
+
+  while (i < len(firstTermDocuments)):
+    totalDocuments.append(firstTermDocuments[i])
+    i += 1
+
+  while (j < len(secondTermDocuments)):
+    totalDocuments.append(secondTermDocuments[j])
+    j += 1
+
+
+  return (totalDocuments)
+  
+def ANDQuery(__firstTerm, __secondTerm):
+  firstTermDocuments = []
+  secondTermDocuments = []
+
+  for document in singleTermQuery(__firstTerm):
+    firstTermDocuments.append(document)
+
+  for document in singleTermQuery(__secondTerm):
+    secondTermDocuments.append(document)
+
+  totalDocuments = []
+  i = 0
+  j = 0
+  
+  while(i < len(firstTermDocuments) and j < len(secondTermDocuments)):
+    if (firstTermDocuments[i] == secondTermDocuments[j]):
+      totalDocuments.append(firstTermDocuments[i])
+      i += 1
+      j += 1
+    elif (firstTermDocuments[i] < secondTermDocuments[j]):
+      i += 1
+    else:
+      j += 1
+
+  return (totalDocuments)
+
+def NOTQuery(__termDocuments):
+  totalDocs = list(range(1, totalNumberOfDocuments + 1))
+  termDocs = []
+  
+  for document in __termDocuments:
+    termDocs.append(document)
+
+  requiredDocs = []
+  i = 0
+  j = 0
+
+  while(i < len(totalDocs) and j < len(termDocs)):
+    if (totalDocs[i] == termDocs[j]):
+      i += 1
+      j += 1
+    elif (totalDocs[i] < termDocs[j]):
+      requiredDocs.append(totalDocs[i])
+      i += 1
+    else:
+      j += 1
+  
+  while(i < len(totalDocs)):
+    requiredDocs.append(totalDocs[i])
+    i += 1
+
+  return (requiredDocs)
+
+def printDocuments(__documents):
+  print("Documents:", end=' ')
+  for document in __documents:
+    print(document, end=',')
+
+def twoTermQuery(__firstTermDocuments, __secondTermDocuments, __operator):
+  if (__operator == 'and'):
+    return (ANDQuery(__firstTermDocuments, __secondTermDocuments))
+  elif (__operator == 'or'):
+    return (ORQuery(__firstTermDocuments, __secondTermDocuments))
+
+def executeQuery(__query):
+  parsedQuery = nltk.word_tokenize(__query)
+  documents = []
+
+  if (len(parsedQuery) == 1):
+    documents = singleTermQuery(parsedQuery[0])
+  elif (len(parsedQuery) == 2 and parsedQuery[0] == 'not'):
+    documents = NOTQuery(singleTermQuery(parsedQuery[1]))
+  elif (len(parsedQuery) == 3):
+    documents = twoTermQuery(parsedQuery[0], parsedQuery[2], parsedQuery[1])
+  elif (len(parsedQuery) == 5):
+    pass
+  
+  printDocuments(documents)
+
+executeQuery('power or play')
+
+# print(dictionary)
